5eb58bc2b6835bb035594e21204c089926fbab69
vincent929 <vincentcheng929@gmail.com>
2019-07-12 10:52:01 +0800

    modifiy model architecture

--- model_definition.py
+++ model_definition.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from config import NUM_CLASSES, image_height, image_width, channels
 
 def create_model():
+    """
     model = tf.keras.Sequential([
         tf.keras.layers.Conv2D(filters=32,
                                kernel_size=(3, 3),
@@ -21,7 +22,14 @@ def create_model():
         tf.keras.layers.Dense(units=NUM_CLASSES,
                               activation=tf.keras.activations.softmax)
     ])
-
+    """
+    input_tensor = tf.keras.layers.Input(shape=(image_height, image_width, channels))
+    model = tf.keras.applications.MobileNetV2(input_shape=(image_height, image_width, channels), input_tensor=input_tensor, include_top=False)
+    avg_pool = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+    dense = tf.keras.layers.Dense(NUM_CLASSES, name='dense')(avg_pool)
+    logits = tf.keras.layers.Activation('softmax', name='logits')(dense)
+    model = tf.keras.models.Model(input_tensor, logits)
+    model.summary()
     model.compile(loss=tf.keras.losses.categorical_crossentropy,
                   optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
                   metrics=['accuracy'])
